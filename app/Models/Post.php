ca519ae82bd708ef536ea69fef8a2f81eafcba26
BenfanteMarco <145206234+BenfanteMarco@users.noreply.github.com>
2024-03-01 02:00:49 +0100

    tables PONTE connected

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Technology;
 
 class Post extends Model
 {
@@ -20,5 +21,8 @@ class Post extends Model
     public function type()
     {
         return $this->belongsTo(Type::class);
+    }
+    public function technologies(){
+        return $this->belongsToMany(Technology::class);
     }
 }
--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Post;
 
 class Technology extends Model
 {
     use HasFactory;
+    protected $fillable =
+    [
+        'name',
+        'slug',
+    ];
+    public function posts(){
+        return $this->belongsToMany(Post::class);
+    }
 }
