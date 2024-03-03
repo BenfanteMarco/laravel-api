958b4f8248f501498b89f028b63d74b3ed009b58
BenfanteMarco <145206234+BenfanteMarco@users.noreply.github.com>
2024-03-03 14:03:12 +0100

    Milestone 2 Done

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -11,6 +11,7 @@ class PostController extends Controller
     public function index(){
         $posts = Post::all();
 
+        // api testate
         return response()->json([
             'success' => true,
             'results' => $posts
