36710621bede0da53eee0d9c0dbe54e9799f6918
BenfanteMarco <145206234+BenfanteMarco@users.noreply.github.com>
2024-03-04 19:00:36 +0100

    added bonus

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -9,7 +9,8 @@ use App\Models\Post;
 class PostController extends Controller
 {
     public function index(){
-        $posts = Post::all();
+        // $posts = Post::all();
+        $posts = Post::with(['type', 'technologies'])->paginate(6);
 
         // api testate
         return response()->json([
